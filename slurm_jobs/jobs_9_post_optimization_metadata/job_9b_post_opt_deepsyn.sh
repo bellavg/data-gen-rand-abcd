feb9d6927a48e0541510d40880f33468e4ff94ae
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2026-03-15 18:53:34 +0100

    deepsyn check for finished

--- slurm_jobs/jobs_9_post_optimization_metadata/job_9b_post_opt_deepsyn.sh
+++ slurm_jobs/jobs_9_post_optimization_metadata/job_9b_post_opt_deepsyn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=9b_post_deepsyn
-#SBATCH --time=04:00:00
+#SBATCH --time=02:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=genoa
@@ -9,5 +9,7 @@
 set -euo pipefail
 
 export ALGORITHM="Deepsyn"
+
+export DESIGN_GROUP="random"
 
 bash "$SLURM_SUBMIT_DIR/slurm_jobs/jobs_9_post_optimization_metadata/job_9_worker_post_optimization_metadata.sh"
