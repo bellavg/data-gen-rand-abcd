f74fada29d0878055ee364613a916fb805d7e242
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2026-03-15 18:49:06 +0100

    shorter time for job 9

--- slurm_jobs/jobs_9_post_optimization_metadata/job_9c_post_opt_syn4.sh
+++ slurm_jobs/jobs_9_post_optimization_metadata/job_9c_post_opt_syn4.sh
@@ -3,7 +3,6 @@
 #SBATCH --time=04:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
 #SBATCH --partition=genoa
 #SBATCH --output=logs/9c_post_syn4_%j.out
 
--- slurm_jobs/jobs_9_post_optimization_metadata/job_9d_post_opt_c2rs.sh
+++ slurm_jobs/jobs_9_post_optimization_metadata/job_9d_post_opt_c2rs.sh
@@ -3,7 +3,6 @@
 #SBATCH --time=04:00:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
 #SBATCH --partition=genoa
 #SBATCH --output=logs/9d_post_c2rs_%j.out
 
