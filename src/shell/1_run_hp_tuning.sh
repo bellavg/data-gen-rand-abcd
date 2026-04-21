99c330b94842640ca11a3a1efb25f1af431d3133
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2026-04-21 16:50:47 +0200

    fix gpu request

--- src/shell/1_run_hp_tuning.sh
+++ src/shell/1_run_hp_tuning.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=aig_optuna
 #SBATCH --time=00:24:00          # 5 days
-#SBATCH --nodes=1                 # Single node
-#SBATCH --ntasks=1
+#SBATCH --N 1
 #SBATCH --cpus-per-task=16
+#SBATCH --partition=gpu_h100
 #SBATCH --gpus=1                  # 1 GPU
 #SBATCH --output=logs/optuna_%j.out
 
