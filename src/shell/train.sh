1d49be855873b846ead95c80450881ab3f607455
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2026-06-09 18:30:38 +0200

    less workers only 8 prefetch of 4, back with torch compile max nodes 2mil

--- src/shell/train.sh
+++ src/shell/train.sh
@@ -130,7 +130,8 @@ srun python -u -m train \
     --tier0_cache_dir   "$TIER0_CACHE_DIR" \
     --tier1_cache_dir   "$TIER1_CACHE_DIR" \
     --hp_tuning_splits_path "$HP_TUNING_SPLITS" \
-    --num_workers       "$NUM_WORKERS" \
+    --prefetch_factor    4 \
+    --num_workers       8 \
     --patience          10
 
 echo "=========================================="
