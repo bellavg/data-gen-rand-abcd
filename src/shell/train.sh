ff64c84fbe49a53693b69518e8cde7aa624d9143
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2026-06-15 11:47:24 +0200

    pin memory true, increase workers and prefecth factor, dercease number of layers from 5 to 4

--- src/shell/train.sh
+++ src/shell/train.sh
@@ -134,8 +134,8 @@ srun python -u -m train \
     --tier0_cache_dir   "$TIER0_CACHE_DIR" \
     --tier1_cache_dir   "$TIER1_CACHE_DIR" \
     --hp_tuning_splits_path "$HP_TUNING_SPLITS" \
-    --prefetch_factor    2 \
-    --num_workers       6 \
+    --prefetch_factor   4 \
+    --num_workers       8 \
     --patience          10
 
 echo "=========================================="
